5aa17e1de13d3b52c9734787992498abdf723af9
lokesh-9839 <72551225+lokesh-9839@users.noreply.github.com>
2022-09-10 21:22:28 +0530

    uploading data and data collection script

--- untitled16.py
+++ untitled16.py
@@ -5,4 +5,4 @@ import pandas as pd
 path = "C:/Users/DELL/Documents/ds_salary_proj/chromedriver"
 df = gs.get_jobs (' dato scientist ', 15 ,False, path, 15)
 
-df
+df.to_csv("glassdoor.jobs_csv",index = False)
