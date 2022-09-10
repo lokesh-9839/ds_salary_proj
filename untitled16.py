3b52cbcac87dc418a1fe8204f9d349745a4ba0f8
lokesh-9839 <72551225+lokesh-9839@users.noreply.github.com>
2022-09-10 20:56:25 +0530

    uploaded scraper and run code

---
+++ untitled16.py
@@ -0,0 +1,8 @@
+import glassdoor_scraper as gs
+
+import pandas as pd
+
+path = "C:/Users/DELL/Documents/ds_salary_proj/chromedriver"
+df = gs.get_jobs (' dato scientist ', 15 ,False, path, 15)
+
+df
